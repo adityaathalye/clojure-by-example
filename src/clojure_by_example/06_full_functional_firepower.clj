ff652180fc144d386f4d5a94429434eeb6ab91b7
Aditya Athalye <aditya.athalye@gmail.com>
2017-12-06 16:55:09 +0530

    Illustrate how to design a Clojure program?

---
+++ src/clojure_by_example/06_full_functional_firepower.clj
@@ -0,0 +1,19 @@
+(ns clojure-by-example.ex06-full-functional-firepower)
+
+;; Ex06: Lesson Goals
+;; - Introduce multi-arity and variadic functions, and also some
+;;   basic de-structuring.
+;; - Explain how we can use these to design flexible function
+;;   "api"s for good "composability"
+;; - Show how to "keep state at the boundary"; e.g.:
+;;   - ingest sophisticated planets from a JSON file
+;;   ----------------Input boundary----------------
+;;   - punch it into a purely functional data processing
+;;   - do awesome things with just `map`, `filter`, `reduce`,
+;;     simple helper functions (with/without control flow)
+;;     and models of the world as pure data structures
+;;   ----------------Output boundary---------------
+;;   - spit out various kinds of outputs into output files
+
+
+;; TBD
