8b8256d63f9763fa997dc5071d4415f8c4055a20
Aditya Athalye <aditya.athalye@gmail.com>
2017-12-06 16:52:34 +0530

    Add 'ex05' draft. Maybe show immutability, FP now?

---
+++ src/clojure_by_example/ex05_immutability_and_fp.clj
@@ -0,0 +1,15 @@
+(ns clojure-by-example.ex05-immutability-and-fp)
+
+;; Ex05: Lesson Goals
+;; - Connect back to previous exercises and show how we did
+;;   whatever we needed, without mutating anything, and without any
+;;   side-effects
+;; - Explain how "pure functions" are just functions of data -> data
+;; - Explain `def`, and why global definitions should be used sparingly
+;; - Introduce let bindings and lexical scope, to show how to keep
+;;   "function-local things", local to the function
+;; - Show where mutability is useful; e.g. I/O
+
+;; TBD
+
+;; Where to explain DEBUGGING and ERROR MESSAGES??!!
