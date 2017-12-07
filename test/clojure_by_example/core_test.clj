bd6db4a7cc9ed2af1e833661310c04c2dbb03ab4
Aditya Athalye <aditya.athalye@gmail.com>
2017-12-07 14:15:27 +0530

    Get rid of dummy core test file

--- test/clojure_by_example/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-by-example.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-by-example.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
