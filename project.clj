93052857dfcbb068650a355cc323c9745f9bd723
Aditya Athalye <aditya.athalye@gmail.com>
2017-12-17 23:35:55 +0530

    Update deps for LightTable + remote nrepl interop

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  ;; LightTable dependencies
+  ;; https://github.com/LightTable/Clojure/#connect-to-remote-nrepl
+  :profiles {:dev {:dependencies [[lein-light-nrepl "0.3.3"]]}}
+  :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]})
