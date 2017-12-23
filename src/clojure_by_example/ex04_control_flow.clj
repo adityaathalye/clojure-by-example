b07262763ad350ad0c22722b04dbb83a02520ab6
Aditya Athalye <aditya.athalye@gmail.com>
2017-12-24 00:37:41 +0530

    Exercise-ify ex04, to eventually colonize planets!

--- src/clojure_by_example/ex04_control_flow.clj
+++ src/clojure_by_example/ex04_control_flow.clj
@@ -137,12 +137,11 @@
 (map    identity
         [1 2 nil 4 5 nil 7 8])
 
-(filter (fn [x] (not (nil? x)))
+(filter (comp not nil?) ; recall: `comp` composes functions into a pipeline
         [1 2 nil 4 5 nil 7 8])
 
 (filter identity
         [1 2 nil 4 5 nil 7 8]) ;; Ha! What happened here?!
-
 
 
 ;; INTERLUDE...
@@ -302,55 +301,250 @@
      [nil 0 1 42])
 
 
+;; EXERCISE:
+;;
+;; Try to reason from first principles:
+;;
+;; Why does cond require `:else` to mark the last / default condition,
+;; but case simply treats the last expression as default?
+;;
+;; (Hint: is `:else` an expression or a value?)
+
+
+;; Clojure hash-sets
+;; - can be used as predicates (and often are used this way)
+
+(= #{:a :b :c}  ; A hash-set of three things, :a, :b, and :c.
+
+   (hash-set :a :b :b :c :a :c :c :c))
+
+
+;; A hash-set behaves like a function to test for set membership.
+
+(#{:a :b :c} :a) ; Does the set contain :a? Truthy.
+
+(#{:a :b :c} :z) ; Does the set contain :z? Falsey.
+
+
+;; How do Clojure programmers use sets as predicates?
+
+(def colonize-it? #{"Earth" "Mars"})
+
+
+((comp colonize-it? :name) {:name "Earth"})
+
+
+((comp colonize-it? :name) {:name "Venus"})
+
+
+(filter (comp colonize-it? :name)
+        [{:name "Mercury"} {:name "Venus"} {:name "Earth"}
+         {:name "Mars"} {:name "Jupiter"}])
 
 
 
 ;; Lesson-end exercise
 
 
+;; LET'S COLONIZE PLANETS!!!!
+;; \\//_
+
+
+(def target-planets
+  [{:name "Mercury"
+    :mass 0.055 :radius 0.383 :moons 0
+    :atmosphere {}} ; empty hash map means no atmosphere
+
+   {:name "Venus"
+    :mass 0.815 :radius 0.949 :moons 0
+    :atmosphere {:carbon-dioxide 96.45 :nitrogen 3.45
+                 :sulphur-dioxide 0.015 :traces 0.095}}
+
+   {:name "Earth" :mass 1 :radius 1 :moons 1
+    :atmosphere {:nitrogen 78.08 :oxygen 20.95 :carbon-dioxide 0.4}
+    :water-vapour 0.10 :argon 0.33 :traces 0.14}
+
+   {:name "Mars" :mass 0.107 :radius 0.532 :moons 2
+    :atmosphere {:carbon-dioxide 95.97 :argon 1.93 :nitrogen 1.89
+                 :oxygen 0.146 :carbon-monoxide 0.056 :traces 0.008}}
+
+   {:name "Chlorine Planet"
+    :mass 2.5 :radius 1.3 :moons 4
+    :atmosphere {:chlorine 100.0}}
+
+   {:name "Insane Planet"
+    :mass 4.2 :radius 1.42 :moons 42
+    :atmosphere {:sulphur-dioxide 80.0 :carbon-monoxide 10.0
+                 :chlorine 5.0 :nitrogen 5.0}}])
+
+
+;; EXERCISE:
+;;
+;; Define a set of `poison-gases`
+;; - Let's say :chlorine, :sulphur-dioxide, :carbon-monoxide are poisons
+
+
+;; EXERCISE:
+;;
+;; Write a "predicate" function to check if a given planet is "Earth".
+;;
+
+(defn earth?
+  [planet]
+  'FIX)
+
+
+;; EXERCISE:
+;;
+;; Write a predicate function to check if a planet has
+;; at least 0.1% :carbon-dioxide in its atmosphere.
+
+(defn carbon-dioxide?
+  [planet]
+  'FIX)
+
+
+;; EXERCISE:
+;;
+;; Having no atmosphere is a bad thing, you know.
+;;
+;; Write a "predicate" function that returns true (or truthy)
+;; if a planet has no atmosphere. It should return false/falsey
+;; if the planet _has_ an atmosphere.
+;;
+;; Call it `no-atmosphere?`
+;;
+;; Use `empty?` to check if the value of :atmosphere is empty.
+(empty? {}) ; is true. It's an empty hash-map.
+;;
+;; Type your solution below
+
+
+;; Quick-n-dirty test
+#_(filter no-atmosphere? target-planets)
 
 
 
-;; Scratch work follows....
+;; EXERCISE:
+;;
+;; Let's say the air is too poisonous if the atmosphere contains
+;; over 1% of _any_ poison gas.
+;;
+;; Write a "predicate" function that checks this, given a planet.
+;;
+;; Call it `air-too-poisonous?`.
 
 
-;; ;; This is a cleaner way to do the same thing...
-;; (filter (comp not planet-has-moons?)
-;;         planets)
 
-;; ;; `comp` is a handy function lets us "compose" or chain other functions
-;; ;; such that the output of the function on the right is connected to the
-;; ;; input of the function on the left.
-;; ;;
-;; ;; So, when you see an expression like:
-;; ;;    ((comp fn1 fn2 fn3 fn4) {:some "data"})
-;; ;;
-;; ;; you can mentally evaluated it like:
-;; ;;    FINAL RESULT <- fn1 <- fn2 <- fn3 <- fn4 <- {:some "data"}
-;; ;;
-;; ;; Which looks suspiciously like a "data pipeline", (or a Unix pipeline
-;; ;; for those familiar with Unix/Linux shell programming)
-
-;; ;; Comp, in fact, returns a general-purpose function, which can take
-;; ;; any input and pass it to the right-most function
-;; (fn? (comp not planet-has-moons?))
-
-;; ;; True, Earth has a moon.
-;; (planet-has-moons?
-;;  {:name "Earth" :moons 1})
-
-;; ;; True, Mercury has no moons.
-;; ((comp not planet-has-moons?)
-;;  {:name "Mercury" :moons 0})
+;; Quick-n-dirty test
+#_(filter air-too-poisonous? planets)
 
 
-;; ;; Back to map...
-;; (map (comp not planet-has-moons?)
-;;      planets)
 
-;; ;; What if a function's output does not match
-;; ;; Well, your function pipeline misbehaves (or could even fail):
+;; EXERCISE:
+;;
+;; Understand the next few functions.
 
-;; ;; When a keyword can't find anything, it returns `nil`
-;; ;; So the whole thing below, returns `nil`, which is useless.
-;; ((comp :name planet-has-moons?) {:name "Earth" :moons 1})
+
+(defn planet-has-some-good-conds?
+  "Given a collection of functions that check a planet for 'good conditions',
+  return true if a given planet satisfies at least one 'good condition'."
+  [good-condition-fns planet]
+  (some (fn [good?] (good? planet))
+        good-condition-fns))
+
+;; Quick-n-dirty test:
+;; - Let's say it's good to be Earth (yay), and
+;; - It's good to have carbon dioxide in the atmosphere.
+
+#_(filter (fn [planet]
+            (planet-has-some-good-conds?
+              [earth? carbon-dioxide?] planet))
+          target-planets)
+
+;; OR we could use `partial`:
+
+#_(filter (partial planet-has-some-good-conds?
+                   [earth? carbon-dioxide?])
+          target-planets)
+
+;; What does `partial` do?
+;; - Retrieve documentation for `partial` using `clojure.repl/doc`.
+;;   (More on "REPL" utilities later.)
+
+(clojure.repl/doc partial) ; evaluate and check the console/repl window
+
+;; Then fix these to make them work:
+
+#_((partial (fn [a b c] (+ a b c))
+            1)
+   'FIX 'FIX)
+
+#_((partial (fn [a b c] (+ a b c))
+            1 2)
+   'FIX)
+
+
+;; EXERCISE:
+;;
+;; Given a collection of bad conditions, and a planet, return true if
+;; the planet has _no_ bad conditions.
+
+(defn planet-has-no-bad-conds?
+  [bad-condition-fns planet]
+  'FIX)
+
+;; Quick-n-dirty test:
+#_(filter (partial planet-has-no-bad-conds?
+                   [air-too-poisonous? no-atmosphere?])
+          target-planets)
+
+#_(filter (complement
+            (partial planet-has-no-bad-conds?
+                     [air-too-poisonous? no-atmosphere?]))
+          target-planets)
+
+
+
+;; EXERCISE:
+;;
+;; Let's say that a habitable planet has some good conditions,
+;; and NO bad conditions.
+;;
+;; Define a function that checks true/truthy for this, given
+;; good-condition-fns, bad-condition-fns, and a planet.
+
+(defn habitable-planet?
+  [FIX]
+  'FIX)
+
+
+;; EXERCISE:
+;;
+;; And finally, write a function that groups a given collection of
+;; planets into :habitable, and :inhospitable.
+;;
+;; The function must internally know what functions will check for
+;; for good conditions, what functions check for bad conditions.
+;;
+;; - Assume it's good to be earth, OR to have carbon-dioxide in the air.
+;; - Assume it's bad to have no atmosphere, or to have poison gases.
+;;
+;; Hint: here's your chance to make good use of `let`, `partial`,
+;; and `complement`.
+
+(defn group-by-habitable
+  [FIX]
+  'FIX)
+
+
+;; Quick-n-dirty test:
+(map (comp (fn [p-name] (str "Send rockets to " p-name " now!"))
+           :name)
+     ((comp :habitable group-by-habitable)
+      target-planets))
+
+
+
+;; RECAP:
+;; FIXME
