94413da1a30e0b5791ebe28166a84bdf660f1f82
Aditya Athalye <aditya.athalye@gmail.com>
2018-01-19 16:00:16 +0530

    Fix project.clj, particularly license notice

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure_by_example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A workshop to introduce Clojure, to programmers new to Clojure."
+  :url "https://github.com/inclojure-org/clojure-by-example"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   ;; LightTable dependencies
   ;; https://github.com/LightTable/Clojure/#connect-to-remote-nrepl
