6b1d8f187d3f4f0ea7e402a7713e61ea4dcfb9d6
Aditya Athalye <aditya.athalye@gmail.com>
2018-10-25 18:14:09 +0530

    WIP: Tighten ex02

--- src/clojure_by_example/ex02_small_functions.clj
+++ src/clojure_by_example/ex02_small_functions.clj
@@ -1,341 +1,278 @@
-(ns clojure-by-example.ex02-small-functions)
+(ns clojure-by-example.ex02-small-functions ; current namespace (ns)
+  ;; "require" and alias another ns as `p`:
+  (:require [clojure-by-example.data.planets :as p]))
+
 
 ;; Ex02: LESSON GOALS
-;;
-;; - Learn to define simple functions, and use them
-;; - Learn a little bit about how functions behave
-;; - Learn about a few useful built-in Clojure functions
-;;   (We will use these in later exercises.)
-;; - Stitch up ideas in this section with a small insight
-;;   into how we can use functions on vectors and hash-maps.
-;;   (Set up your intuition, for later exercises.)
+;; - Use stuff we've seen so far to build purely functional logic
+;;   to process a bunch of planets
 
 
-;; First, some simple functions:
+;; Here are some target planets:
+clojure-by-example.data.planets/target-planets
 
+;; Which we can access more conveniently as:
+p/target-planets
 
-;; We define functions like this:
-;;
-;; (defn function-name
-;;   "Documentation string (optional)."
-;;   [arg1 arg2 arg3 ... argN]
-;;   (body of the function))
+(map :pname p/target-planets)
 
+;; Now, let's colonize planets!
 
-;; `defn` stands for:
-;; - "DEfine a FuNction,
-;; - _and_ give it a globally-referenced name"
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Some constants and utilities
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn same
-  "Simply return the input unchanged."
-  [x]
-  x)
 
-;; EXERCISE
-;;
-;; What will these return?
+(def tolerances
+  "Define low/high bounds of planetary characteristics we care about."
+  {:co2                {:low 0.1,  :high 5.0}
+   :gravity            {:low 0.1,  :high 2.0}
+   :surface-temp-deg-c {:low -125, :high 60}})
 
-(same 42)
 
-(same {:pname "Earth" :moons 1})
+(defn lower-bound
+  [tolerance-key]
+  (get-in tolerances [tolerance-key :low]))
 
-(same [1 2 3 4 5])
 
+(defn upper-bound
+  [tolerance-key]
+  (get-in tolerances [tolerance-key :high]))
 
-;; We can define name-less functions too, like this:
-;;
-;; (fn [arg1 arg2 arg3 ... argN]
-;;     (body of the function))
-;;
-;; `fn` is short for "define a FuNction, but _do not_ name it at all"
-;;
-;; E.g. This behaves _exactly_ like `same`, but it does not have a name:
 
-(fn [x] x)
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Functions to test a planet for particular conditions
+;; - We make them return truthy/falsey values
+;; - We call such functions "predicates"
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn atmosphere-present?
+  [{:keys [atmosphere] :as planet}]
+  (not-empty atmosphere))
 
+#_(map :pname
+       (filter atmosphere-present? p/target-planets))
 
-;; EXERCISE:
-;;
-;; What will these return?
-;; - Hint: _mentally replace_ the function definition `(fn [x] x)`
-;;   with _any name you like_, and imagine that is the function name.
-;;   BUT make sure it behaves just like `(fn [x] x)`.
 
-((fn [x] x) 42)
+(defn co2-tolerable?
+  [{{:keys [carbon-dioxide]
+     :or {carbon-dioxide 0.0}} :atmosphere
+    :as planet}]
+  (<= (lower-bound :co2)
+      carbon-dioxide
+      (upper-bound :co2)))
 
-((fn [x] x) {:pname "Earth" :moons 1})
+#_(map :pname
+       (filter co2-tolerable? p/target-planets))
 
-((fn [x] x) [1 2 3 4 5])
 
+(defn gravity-tolerable?
+  [{:keys [gravity] :as planet}]
+  (when gravity
+    (<= (lower-bound :gravity)
+        gravity
+        (upper-bound :gravity))))
 
-;; We will make good use of such "anonymous" functions soon.
-;; Just understand they are just like 'regular' functions, except
-;; they do not have a globally-referenced name (hence "anonymous").
+#_(map :pname
+       (filter gravity-tolerable? p/target-planets))
 
 
-;; Reminder: Do More With Less!
-;; - Observe that `same` as well as `(fn [x] x)` appear to be capable
-;;   of accepting _any_ kind of value and returning it.
-;; - This function definition is very general-purpose indeed.
-;; - We will frequently use this "general-purpose" idea.
+(defn surface-temp-tolerable?
+  [{{:keys [low high]} :surface-temp-deg-c
+    :as planet}]
+  (when (and low high)
+    (<= (lower-bound :surface-temp-deg-c)
+        low
+        high
+        (upper-bound :surface-temp-deg-c))))
 
+#_(map :pname
+       (filter surface-temp-tolerable? p/target-planets))
 
-;; Some "built-in" functions
 
-;; Clojure has a function called `fn?`, that returns true if we
-;; pass it something that is a function, and false otherwise.
+(def poison-gas?
+  "A set of poison gases."
+  #{:chlorine, :sulphur-dioxide, :carbon-monoxide})
 
-;; None of these are functions. So, return `false`.
 
-(fn? 42)
 
-(fn? "moons")
+;; We can use sets as predicates. Sets behave as functions
+;; that test for set membership.
 
-(fn? [1 2 3 4 5])
+(poison-gas? :oxygen)   ; falsey
 
+(poison-gas? :chlorine) ; truthy
 
-;; We defined the function `same`, and truly, it is a function...
-(fn? same)
 
-;; Our "anonymous" function, is a function too. But of course!
-(fn? (fn [x] x))
+(defn air-too-poisonus?
+  [{:keys [atmosphere] :as planet}]
+  (some (fn [[gas-name gas-pct]]
+          (and (poison-gas? gas-name)
+               (<= 1.0 gas-pct)))
+        atmosphere))
 
-;; And `fn?` itself is a... ______ ?
-(fn? fn?)
-
-
-;; Wait a minute...!!!
-;;
-;; We just passed functions as arguments to `fn?`, and it worked!
-;;
-;; Is `fn?` special, or can we pass any function to any function?
+(map :pname
+     (filter air-too-poisonus? p/target-planets))
 
-;; Well we know `fn?` is a function...
-;; - Suppose we pass `fn?` to `same`, do we get back `fn?` unchanged?
+;; a hash-map is a collection of key-value pairs
+(map identity
+     {:nitrogen 78.08, :oxygen 20.95, :carbon-dioxide 0.4,
+      :water-vapour 0.1, :argon 0.33, :traces 0.14})
 
-(= fn?
-   (same fn?))
+(map (fn [[k v]] (str v "% " k))
+      {:nitrogen 78.08, :oxygen 20.95, :carbon-dioxide 0.4,
+       :water-vapour 0.1, :argon 0.33, :traces 0.14})
 
 
-;; How about the anonymous version of `same`?
-;; Does it return `fn?` unchanged as well?
-(= 'FIX
-   ((fn [x] x) fn?))
 
-;; How about this?
-;; What do we get if we pass `same` to itself?
-(= 'FIX
-   (same same))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Composite checks to
+;; - test whether a given planet meets a variety of conditions.
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;; Lesson: Functions also behave like "values":
-;;
-;; - `42` is a value. `"moons"` is a value. `:moons` is a value.
-;;   - Values represent themselves directly
-;;   - Values are unique in the whole universe (42 is NOT "forty two")
-;;   - Values never change. They are constant forever and ever.
-;;
-;; - All Clojure function also are "values", including user-defined
-;;   functions (like `same`). And, just like the other values...
-;;   - We can pass functions as arguments to other functions,
-;;     without any special syntax or declarations.
-;;   - Functions can _return_ functions as results.
-;;   - We can compare any two functions and tell if they are
-;;     exactly the same thing.
+(defn planetary-conditions-meet-constraint?
+  "Return the given planet as-is, when it meets the condition fns,
+  as per the constraint fn (like every?, some, not-every).
 
+  The constraint fn must return a Boolean true/false value."
+  [constraint]
+  (fn [conditions planet]
+    (when (true? (constraint (fn [f] (f planet))
+                             conditions))
+      planet)))
 
-;; Some other "built-in" Clojure functions
 
-;; Return `true` if even, `false` otherwise
-(even? 2)
+(def planet-meets-any-one-condition?
+  (planetary-conditions-meet-constraint?
+   some))
 
-;; Return `true` if odd, `false` otherwise
-(odd? 3)
 
-;; Increment by one
-(inc 42)
+(def planet-meets-all-conditions?
+  (planetary-conditions-meet-constraint?
+   every?))
 
-;; Clojure's `identity` function is exactly like our `same` function.
-;; - To prove it, fix the s-expression below so it evaluates to `true`:
-(= 'FIX
-   (identity :moon)
-   (same :moon))
-;; We will use `identity` in surprisingly useful ways later.
 
+(def planet-meets-no-condition?
+  (planetary-conditions-meet-constraint?
+   not-any?))
 
 
-;; Now, let's use simple functions to process Collections
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Conditions for colonisation
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;; `map`
-;;
-;; `map` a function over a collection...
-(map inc [1 2 3 4 5 6])
-;; Such that...
-#_( 1 2 3 4 5 6)  ; each item of input
-;;  | | | | | |   ; is incremented by `inc` to give a result where
-#_( 2 3 4 5 6 7)  ; each output item "maps" back to an input item
+(def minimal-good-conditions
+  "A collection of functions that tell us about the
+  good-ness of planetary conditions."
+  [co2-tolerable?
+   gravity-tolerable?
+   surface-temp-tolerable?])
 
-;; Note:
-;; - Ignore the following subtlety for now:
-;;   You may have noticed that we passed a vector [] to `map` above,
-;;   but the result looks like a list (). Well it really isn't a
-;;   concrete list, but a "sequence" representation of the vector.
-;;   Clojure wraps a "sequence" in parens (), for display purposes only.
-;;
-;; - Think of `map` in general terms, as a way to express
-;;   one-to-one "mappings" of an input sequence to an output sequence,
-;;   by way of a function.
-;;
-;; - The syntax of `map` is:
-;;
-;;   (map your-function input-collection)
-;;
-;;   Where 'your-function' must accept exactly one argument, because
-;;   it must transform only one item of the input at a time.
 
+(def fatal-conditions
+  "A collection of functions that tell us about the
+  fatality of planetary conditions."
+  [(complement atmosphere-present?)
+   air-too-poisonus?])
 
-;; EXERCISE:
-;;
-;; What should the following `map` expression return?
-;; - First predict the answer, then evaluate to confirm.
-;; - Hint: mentally apply the `even?` function to each item, one by one,
-;;   and build up a collection of results of each function application.
-;; - And ignore the [] v/s () subtlety of input v/s output display.
-;;   Just predict the sequence of output items, in the correct order.
 
-(map even? [1 2 3 4 5 6])
-
-
-;; How about this?
-
-(map odd? [1 2 3 4 5 6])
-
-
-;; And this?
-
-(map identity [1 2 3 4 5 6]) ; Recall: `identity` is just like `same`
-
-
-;; And this?
-
-(map (fn [x] x) [1 2 3 4 5 6])
-
-;; Nice! Our anonymous "identity" function is a drop-in replacement
-;; for `identity`, as well as `same`.
-
-
-
-;; Now for a little bit of fun with `map` and some planets.
-
-
-;; This is a Clojure vector... of numbers
-
-[1 2 3 4 5 6]
-
-
-;; This is a Clojure vector... of Clojure hash-maps.
-
-[{:pname "Mercury" :moons 0}
- {:pname "Venus"   :moons 0}
- {:pname "Earth"   :moons 1}
- {:pname "Mars"    :moons 2}] ; (Yes we can do this. More on this later!)
-
-
-;; Let's name our collection of planets as, um... `planets`
-
-(def planets [{:pname "Mercury" :moons 0}
-              {:pname "Venus"   :moons 0}
-              {:pname "Earth"   :moons 1}
-              {:pname "Mars"    :moons 2}])
-
-
-;; Recall that we can query a map, like this:
-
-(:pname {:pname "Mercury" :moons 0})
-
-;; That is, a keyword _behaves like a function_ of a hash-map.
-
-
-;; EXERCISE
-;;
-;; What if we pass a keyword instead of a real function, to `map`?
-;; - What should the following map expression return?
-;; - Predict the answer, and then evaluate to confirm.
-
-(map :pname planets)
-
-;; Read as:
-;; "map :pname over `planets`, which is vector of planet hash-maps"
-
-
-
-;; Stitching it all together...
-
-
-;; Let's define another little function.
-
-(defn planet-has-moons?
-  "Given a 'planet' (assume it's a hash-map), return true if
-  it has at least one moon."
+(defn habitable?
+  "We deem a planet habitable, if it has all minimally good conditions,
+  and no fatal conditions."
   [planet]
-  (> (:moons planet)
-     0))
-
-(planet-has-moons? {:pname "Mercury" :moons 0})
-
-(planet-has-moons? {:pname "Earth" :moons 1})
-
-(planet-has-moons? {:pname "Mars" :moons 2})
+  (planet-meets-no-condition?
+   fatal-conditions
+   (planet-meets-all-conditions?
+    minimal-good-conditions
+    planet)))
 
 
-;; EXERCISE
-;;
-;; Instead of querying each map, why not query all of them at one go?
-
-#_(map 'FIX 'FIX)
+#_(map :pname
+       (filter habitable? p/target-planets))
 
 
-;; Also, as we now know, we can use anonymous functions creatively...
+(defn colonisable?
+  "We deem a planet colonisable, if it has at least one
+  minimally good condition, and no fatal conditions."
+  [planet]
+  (and (planet-meets-any-one-condition?
+        minimal-good-conditions
+        planet)
+       (planet-meets-no-condition?
+        fatal-conditions
+        planet)))
+
+#_(map :pname
+       (filter colonisable? p/target-planets))
 
 
-;; EXERCISE
-;;
-;; Replace 'FIX with your own anonymous function that works
-;; just like `planet-has-moons?`.
+(defn observe-only?
+  "We select a planet for orbital observation, if it only has harsh surface conditions."
+  [planet]
+  (and (planet-meets-any-one-condition?
+        fatal-conditions
+        planet)
+       (planet-meets-no-condition?
+        minimal-good-conditions
+        planet)))
 
-#_(map 'FIX  planets)
-
-
-;; EXERCISE
-;;
-;; And, finally, prove that both variants do exactly the same thing:
-
-#_(= (map planet-has-moons? 'FIX)
-
-     ('FIX 'FIX 'FIX)  ; use anonymous function
-
-     [false false true true])
+#_(map :pname
+       (filter observe-only? p/target-planets))
 
 
 
-;; RECAP:
-;; - Functions are easy to define
-;; - Functions can _accept_ functions as arguments
-;; - Functions can _return_ functions as arguments
-;; - Clojure has nifty "built-in" functions
-;;   - e.g. the function `map` lets us express a mapping of an
-;;     input collection to an output collection, by way of a function.
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enrich planetary data with analytical results
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;; "Do more with less":
-;; - Even the most simple functions can be general-purpose
-;; - Clojure keywords behave like functions of hash-maps
-;; - Since keywords can query hash-maps, we can completely avoid writing
-;;   custom "getter" functions to query values in our hash-maps.
-;; - We can put hash-maps in vectors, to make vectors of hash-maps.
-;; - Since `map` accepts keywords in place of functions, we can
-;;   combine the above tiny set of ideas, to query many planetary hash-
-;;   maps at one go.
+(defn analyze-planet
+  [planet]
+  (cond
+    (habitable? planet) :inhabit
+    (colonisable? planet) :colonise
+    (observe-only? planet) :observe-only
+    :else :send-probes))
+
+
+#_(map (juxt :pname analyze-planet)
+       p/target-planets)
+
+(map analyze-planet p/target-planets)
+
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Starfleet assigns mission vessels...
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defmulti assign-vessels ; multi-method definition
+  analyze-planet)        ; dispatch function
+
+
+(defmethod assign-vessels :inhabit [planet]
+  (assoc planet
+         :vessels {:starships 5, :orbiters 5, :probes 100,
+                   :battle-cruisers 5, :cargo-ships 5}))
+
+
+(defmethod assign-vessels :colonise [planet]
+  (assoc planet
+         :vessels {:starships 1, :probes 50}))
+
+
+(defmethod assign-vessels :send-probes [planet]
+  (assoc planet
+         :vessels {:orbiters 1, :probes 10}))
+
+
+(defmethod assign-vessels :observe-only [planet]
+  (assoc planet
+         :vessels {:orbiters 1}))
+
+
+#_(map (juxt :pname analyze-planet :vessels)
+       (map assign-vessels p/target-planets))
